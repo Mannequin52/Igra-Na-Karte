a5e6e9b982c868d1637e02998cc45c8e562e1159
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-05-03 09:28:34 +0200

    Add files via upload

---
+++ Igra na karte/Igra na karte/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igra_na_karte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi igrače karte (Vrijednosti 1 do 13)!");
+            int karta, zbroj=0;
+            do
+            { 
+                karta = Convert.ToInt32(Console.ReadLine());
+                zbroj = zbroj + karta;
+            } while (zbroj != 31 || zbroj > 31);
+            if (zbroj == 31)
+            {
+                Console.WriteLine("Pobijedili ste");
+            }
+            else
+            {
+                Console.WriteLine("izgubili ste!");
+            }
+        }
+    }
+}
